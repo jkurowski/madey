54d77e03c367b0220a78367c56656f3af6486915
Jacek <kontakt@jk-design.pl>
2022-12-05 12:51:04 +0100

    Zmiany Dominika

--- library/kCMS/Site.php
+++ library/kCMS/Site.php
@@ -25,7 +25,6 @@ abstract class kCMS_Site extends Zend_Controller_Action {
 		//$this->view->finwestycje = $db->fetchAll($inwest);
 	
 		$menu = new kCMS_MenuBuilder();
-        ray($menu);
 		Zend_Registry::set('querymenu', $menu);
 		
 		$inlineArray = $db->fetchAll($db->select()->from('inline'));
